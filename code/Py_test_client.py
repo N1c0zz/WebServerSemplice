2a071e941105703bda11b85b5e23baf982cf6027
Nicolo Morini <nicolo.morini2@studio.unibo.it>
2024-05-13 14:36:38 +0200

    Modified code comments

--- code/Py_test_client.py
+++ code/Py_test_client.py
@@ -12,15 +12,12 @@ def ConnectToServer():
 
     server_ip = 'localhost'
     server_port = 8080
-    #Create a socket instance
+
     socketObject = socket.socket()
 
-    #Using the socket connect to a server...in this case localhost
     socketObject.connect((server_ip, server_port))
 
     print("Connected to: ", server_ip)
-
-    # Send a message to the web server to supply a page as given by Host param of GET request
 
     HTTPMessage = "GET /pagina_html_prova.html HTTP/1.1\r\n Connection: close\r\n\r\n"
 
@@ -28,7 +25,6 @@ def ConnectToServer():
 
     socketObject.sendall(bytes)
     
-    # Receive the data
     while(True):
 
         data = socketObject.recv(1024)
@@ -49,7 +45,6 @@ for index in range(ThreadCount):
     ThreadList.append(ThreadInstance)
     ThreadInstance.start()
 
-# Main thread to wait till all connection threads are complete
 for index in range(ThreadCount):
 
     ThreadList[index].join()
